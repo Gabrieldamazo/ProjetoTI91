dbfdce3e6ce2e38325e839476fc049f008e3bffa
GABRIEL DAMAZO DOS SANTOS <gabriel.dsantos8@senacsp.edu.br>
2019-03-29 12:11:05 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/projetoti91/SplashActivity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.projetoti91;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
